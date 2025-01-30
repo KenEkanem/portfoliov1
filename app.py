4fd254ea04ada363a184f8db862707b450fe2604
KenEkanem <kennedyekanem@gmail.com>
2025-01-30 06:54:56 +0100

    Added New mono font (Andika)
    - ytnusic icon
    - Update script for card switch
    - Added project back view/content | Update page

--- app.py
+++ app.py
@@ -6,5 +6,15 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/sortapp')
+def sortapp():
+    return render_template('sortapp.html')
+
+
+@app.route('/landest')
+def landest():
+    return render_template('landest.html')
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5003, debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,11 @@ const scrollArrow = document.querySelector('.scroll-arrow img');
 scrollArrow.addEventListener('click', () => {
     document.getElementById('main-content').scrollIntoView({ behavior: 'smooth' });
 });
+
+document.querySelectorAll('.project-item').forEach(item => {
+    item.addEventListener('click', () => {
+        item.classList.toggle('flipped');
+    });
+});
+
+  
